cd677cc6aeccb70554f42351de3f55a2ea2ebf73
Administrator <administrator@gbtc408015dt.montgomerycollege.edu>
2018-06-21 16:50:12 -0400

    updated

--- basicAlert/ViewController.swift
+++ basicAlert/ViewController.swift
@@ -9,7 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var doAlert: UIView!
+    
+    @IBAction func doAlert(_ sender: Any) {
+        let alertController = UIAlertController(title: "Alert Me Button Selected",
+                                                message: "I need your attention NOW!",
+                                                preferredStyle: UIAlertControllerStyle.alert)
+        let defaultAction = UIAlertAction(title: "OK",
+                                          style: UIAlertActionStyle.cancel,
+                                          handler: nil)
+        alertController.addAction(defaultAction)
+        
+        present(alertController, animated:true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
